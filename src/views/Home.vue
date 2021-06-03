ae2a8abdcafe3b8bfb83d3ae5e696097bf19a558
dani <dani.zaghian@gmail.com>
2021-06-03 12:07:49 -0700

    adds axios post request to create recipe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+    <h2>New Recipe</h2>
+    <button v-on:click="createRecipe()">Add Recipe</button>
+
+    <div v-for="recipe in recipes" :key="recipe.id">
       <h3>Title: {{ recipe.title }}</h3>
+      <img :src="recipe.image_url" alt="" />
       <p>Ingredients: {{ recipe.ingredients }}</p>
       <p>Directions: {{ recipe.directions }}</p>
       <p>Prep Time: {{ recipe.friendly_prep_time }}</p>
@@ -9,7 +13,11 @@
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 250px;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -28,6 +36,24 @@ export default {
         console.log(response.data);
         this.recipes = response.data;
       });
+    },
+    createRecipe: function () {
+      var params = {
+        directions: "Make filling, make crust, put filling in crust, bake",
+        ingredients: "Strawberries, rhubarb, pie crust",
+        prep_time: 45,
+        image_url:
+          "https://www.simplyrecipes.com/thmb/e2zSk7O0gwgAyjujpoGx7DbTKC8=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2014__06__strawberry-rhubarb-pie-vertical-a-1600-b5f28ee80738408e8d34ad629a0b431c.jpg"
+      };
+      axios
+        .post("http://localhost:3000/recipes", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.recipes.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
     }
   }
 };
