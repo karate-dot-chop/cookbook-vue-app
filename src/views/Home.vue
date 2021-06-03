624626c1616c6f4687d2f4af0d0a1cb0e60bd65f
dani <dani.zaghian@gmail.com>
2021-06-03 10:52:03 -0700

    adds recipes index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="home">
-    <h1>{{ message }} and my name is {{ name }}</h1>
-    <button v-on:click="sayMyName()">Click me</button>
+    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+      <h3>Title: {{ recipe.title }}</h3>
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
+      <p>Prep Time: {{ recipe.friendly_prep_time }}</p>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      name: "Dani"
+      recipes: []
     };
   },
-  created: function () {},
+  created: function () {
+    this.indexRecipes();
+  },
   methods: {
-    sayMyName: function () {
-      console.log(this.name);
+    indexRecipes: function () {
+      axios.get("http://localhost:3000/recipes").then((response) => {
+        console.log(response.data);
+        this.recipes = response.data;
+      });
     }
   }
 };
