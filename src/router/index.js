19c50410e9e55a2cecc1fb418e2404f144f83e26
dani <dani.zaghian@gmail.com>
2021-06-07 14:09:26 -0700

    adds signup page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Test from "../views/Test.vue";
+import Signup from "../views/Signup.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/test",
     name: "Test",
     component: Test
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup
   }
 ];
 
