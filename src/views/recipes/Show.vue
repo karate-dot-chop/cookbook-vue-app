acc64cdc48d2f14442a6cc48454103fdedeecf83
dani <dani.zaghian@gmail.com>
2021-06-10 11:41:16 -0700

    adds destroy recipe method to show pagedata confirm

--- src/views/recipes/Show.vue
+++ src/views/recipes/Show.vue
@@ -5,7 +5,8 @@
     <p>{{ recipe.ingredients }}</p>
     <p>{{ recipe.directions }}</p>
     <p>Added by: {{ recipe.user.name }} on {{ recipe.friendly_created_at }}</p>
-    <router-link :to="`/recipes/${recipe.id}/edit`">Edit</router-link>
+    <router-link :to="`/recipes/${recipe.id}/edit`">Edit</router-link> |
+    <button v-on:click="destroyRecipe()">Delete</button>
   </div>
 </template>
 
@@ -22,6 +23,16 @@ export default {
       console.log("Recipe object", response.data);
       this.recipe = response.data;
     });
+  },
+  methods: {
+    destroyRecipe: function () {
+      if (confirm("Are you sure you want to delete this recipe?")) {
+        axios.delete(`/recipes/${this.recipe.id}`).then((response) => {
+          console.log(response.data);
+          this.$router.push("/recipes");
+        });
+      }
+    }
   }
 };
 </script>
