0eb2c5ce3b958958c9151a4ba96f646ae137c7ff
dani <dani.zaghian@gmail.com>
2021-06-17 09:41:07 -0700

    adds markers and popup

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -20,12 +20,35 @@ export default {
   },
   mounted: function () {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
-    new mapboxgl.Map({
+    var map = new mapboxgl.Map({
       container: "map", // container id
       style: "mapbox://styles/mapbox/streets-v11", // style URL
       center: [-87.62, 41.88], // starting position [lng, lat]
-      zoom: 15 // starting zoom
+      zoom: 10 // starting zoom
     });
+
+    // create the popup
+    var popup = new mapboxgl.Popup({ offset: 30 }).setText(
+      "Chicago is the windy city with lots of pizza."
+    );
+
+    // Create a default Marker and add it to the map.
+    var marker1 = new mapboxgl.Marker()
+      .setLngLat([-87.62, 41.88])
+      .setPopup(popup)
+      .addTo(map);
+    console.log(marker1);
+
+    // Create a default Marker, colored black, rotated 45 degrees.
+    var marker2 = new mapboxgl.Marker({
+      color: "black",
+      rotation: 45,
+      isDraggable: true,
+      draggable: true
+    })
+      .setLngLat([-122.431297, 37.773972])
+      .addTo(map);
+    console.log(marker2);
   },
   methods: {}
 };
